2ccc5504221831d213440bc0695866dca932388c
Yansa27 <juliansa837@gmail.com>
2023-01-21 23:07:32 +0700

    push

---
+++ script.js
@@ -0,0 +1,61 @@
+console.log("hello world");
+
+// console.log(5 + 5);
+
+// cara bikin variabel
+// var yourname = "yunita";
+
+// var yourname = "bangkit";
+
+// let yourgae = 18;
+// console.log(yourgae);
+// yourgae = 15;
+// console.log(yourgae);
+
+// let number ;
+// const huruf = "hallo deck";
+// number = 10;
+
+// console.log(typeof(number));
+
+// console.log(typeof(huruf));
+
+// ! Tipe Tipe operator
+// let number = 10;
+// let angka = 5;
+
+// let hasil = number * angka;
+// console.log(hasil);
+// hasil = number + number;
+// console.log(hasil);
+// hasil = number - number;
+// console.log(hasil);
+// hasil = number % number;
+// console.log(hasil);
+
+// console.log()
+
+// let a = 10;
+// let b = 5;
+// console.log(10 + 10);
+// console.log(10 - 10);
+// console.log(10 / 10);
+// console.log(a % b);
+// console.log(a**2);
+
+// let x = 10;
+
+// console.log(x)
+console.log(10++);
+console.log(x);
+console.log(++x);
+console.log(x);
+
+
+
+
+
+
+
+
+

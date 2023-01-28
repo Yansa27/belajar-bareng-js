1c1a1ef8eda1efc2f5aa15f54e46a0b2c5d3a3da
Yansa27 <juliansa837@gmail.com>
2023-01-28 22:48:25 +0700

    push

---
+++ belajar2.js
@@ -0,0 +1,43 @@
+// var 
+// let Nama = "juliansa";
+// const Nilai = 20;
+
+// Numbers , string , Null , undefine
+
+// operator = + , - , / , % , * , **
+
+// let number = 11;
+
+// if (number == 10) {
+    
+// } else if (number == 11) {
+//     console.log("ini 11");
+// }else {
+//     console.log("salah");
+// }
+
+
+// let number = 10
+
+// switch (number) {
+//     case 13 :
+//         console.log("ini yang pertama");
+//         break;
+//     case 10 : 
+//         console.log("ini yang kedua");
+//         break;
+// }
+
+let number = 100
+
+for (let a = 10; a < number ; a++) {
+    console.log(`angka ${a}`);
+}
+
+
+
+
+
+
+
+

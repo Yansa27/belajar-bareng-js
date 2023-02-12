55a5c91d2d1a247162870de8ba1f5ca5e4b899d2
Yansa27 <juliansa837@gmail.com>
2023-02-12 22:54:12 +0700

    push

---
+++ callback.js
@@ -0,0 +1,14 @@
+const ordercoffe = callback => {
+    let coffe = null
+    console.log("silahkan menunggu...");
+
+    setTimeout(() => {
+        coffe = "berhasil di buat";
+
+        callback (coffe);
+    } , 3000)
+}
+
+ordercoffe(coffe => {
+    console.log(coffe);
+})
